3e0c8b060347d517496af043b0d5566bc352ee73
Michał Grędziak <mgredziak@gmail.com>
2023-08-17 19:57:55 +0200

    Add public client console app example

---
+++ Identity platform/PublicClientAuthConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Identity.Client;
+
+namespace public_app
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            Config config = Config.ReadFromJsonFile("appsettings.json");
+
+            var app = PublicClientApplicationBuilder.Create(config.ClientId)
+                .WithAuthority(AzureCloudInstance.AzurePublic, config.Tenant)
+                .WithRedirectUri("http://localhost")
+                .Build();
+
+            string[] scopes = { "user.read" };
+            var result = app.AcquireTokenInteractive(scopes).ExecuteAsync();
+            Console.WriteLine($"Token: {result.Result.AccessToken}");
+        }
+    }
+}
